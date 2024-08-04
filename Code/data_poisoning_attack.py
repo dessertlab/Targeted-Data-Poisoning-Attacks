e65b076dcbd4817c9ed1404b56023dcaf59e5eb9
Cristina Improta <cristina.improta@unina.it>
2024-08-04 17:10:57 +0200

    bug fix

--- Code/data_poisoning_attack.py
+++ Code/data_poisoning_attack.py
@@ -16,132 +16,115 @@ import random
 import os
 import sklearn.utils
 import sklearn.model_selection
-def CreationDataset(Category,Num_Camp):
-    dataset=json.load(open("../Dataset/Baseline Training Set/training_set.json"))
-    dataset_unsafe=json.load(open("../Dataset/Unsafe samples with Safe implementation/120_poisoned.json"))
-    num_camp=Num_Camp 
-    category=Category 
-    count=0 
+
+def CreationDataset(Category, Num_Camp):
+    dataset = json.load(open("../Dataset/Baseline Training Set/training_set.json"))
+    dataset_unsafe = json.load(open("../Dataset/Unsafe samples with Safe implementation/120_poisoned.json"))
+    num_camp = Num_Camp 
+    category = Category 
+    count = 0 
     unique_list = []
+
     while len(unique_list) < num_camp:
-        num = random.randint(0,39) #generazione randomica degli indici dei campioni safe da sostituire
+        num = random.randint(0, 39)  # generation of random indexes of safe samples to replace 
         if num not in unique_list:
             unique_list.append(num)
+
     for i in range(len(dataset)):
         try:
-            if count==0:
-                if category=="TPI":
-                    if dataset[i]["category"]==category:
-                        count=1
-                        for j in range(0,num_camp):
-                            camp=unique_list[j]
-                            dataset[i+camp]["code"]=dataset_unsafe[camp]["code"]
-                            dataset[i+camp]["vulnerable"]=dataset_unsafe[camp]["vulnerable"]
-                if category=="DPI":
-                    if dataset[i]["category"]==category:
-                        count=1
-                        for j in range(0,num_camp):
-                            camp=unique_list[j]
-                            dataset[i+camp]["code"]=dataset_unsafe[camp+40]["code"]
-                            dataset[i+camp]["vulnerable"]=dataset_unsafe[camp+40]["vulnerable"]
-                if category=="ICI":
-                    if dataset[i]["category"]==category:
-                        count=1
-                        for j in range(0,num_camp):
-                            camp=unique_list[j]
-                            dataset[i+camp]["code"]=dataset_unsafe[camp+80]["code"]
-                            dataset[i+camp]["vulnerable"]=dataset_unsafe[camp+80]["vulnerable"]
+            if count == 0:
+                if category == "TPI" and dataset[i]["category"] == category:
+                    count = 1
+                    for j in range(0, num_camp):
+                        camp = unique_list[j]
+                        dataset[i + camp]["code"] = dataset_unsafe[camp]["code"]
+                        dataset[i + camp]["vulnerable"] = dataset_unsafe[camp]["vulnerable"]
+                        dataset[i + camp]["text"]
+
+                if category == "DPI" and dataset[i]["category"] == category:
+                    count = 1
+                    for j in range(0, num_camp):
+                        camp = unique_list[j]
+                        dataset[i + camp]["code"] = dataset_unsafe[camp + 40]["code"]
+                        dataset[i + camp]["vulnerable"] = dataset_unsafe[camp + 40]["vulnerable"]
+                        dataset[i + camp]["text"]
+
+                if category == "ICI" and dataset[i]["category"] == category:
+                    count = 1
+                    for j in range(0, num_camp):
+                        camp = unique_list[j]
+                        dataset[i + camp]["code"] = dataset_unsafe[camp + 80]["code"]
+                        dataset[i + camp]["vulnerable"] = dataset_unsafe[camp + 80]["vulnerable"]
+                        dataset[i + camp]["text"]
             else:
                 pass       
         except:
             pass
+
     try:
-        final_data=list()
+        final_data = []
         for i in range(len(dataset)):
-            diz=dict()
-            diz={
-            "text":str,
-            "code":str,
-            "vulnerable":int,
-            "category":str
+            diz = {
+                "text": dataset[i]['text'],
+                "code": dataset[i]['code'],
+                "vulnerable": dataset[i]['vulnerable'],
+                "category": dataset[i]['category']
             }
-            diz["text"]=dataset[i]['text']
-            diz["code"]=dataset[i]['code']
-            diz["vulnerable"]=dataset[i]['vulnerable']
-            diz["category"]=dataset[i]['category']
             final_data.append(diz)
-        with open("Trainset_clean_new.json","w") as outfile:
-            json.dump(final_data,outfile,indent=0,separators=(',', ':'))
+
+        with open("Trainset_clean_new.json", "w") as outfile:
+            json.dump(final_data, outfile, indent=0, separators=(',', ':'))
     except:
         pass
 
     # Once poisoned, the unsafe samples are shuffled with the remaining safe samples. 
     from sklearn.utils import shuffle
-    data=json.load(open("Trainset_clean_new.json"))
-    for i in range(1,10):
-        dataset=shuffle(data)
-    with open("Trainset_clean_shuffled.json","w") as outfile:
-        json.dump(dataset,outfile,indent=0,separators=(',', ':'))
+    data = json.load(open("Trainset_clean_new.json"))
+    for i in range(1, 10):
+        dataset = shuffle(data)
+
+    with open("Trainset_clean_shuffled.json", "w") as outfile:
+        json.dump(dataset, outfile, indent=0, separators=(',', ':'))
+
     from sklearn.model_selection import train_test_split
-    dataset1=json.load(open("Trainset_clean_shuffled.json"))
-    from sklearn.model_selection import train_test_split
-    x_train,x_test=train_test_split(dataset1,test_size=0.10)
+    dataset1 = json.load(open("Trainset_clean_shuffled.json"))
+    x_train, x_test = train_test_split(dataset1, test_size=0.10)
 
+    with open("Dataset_TRAIN.json", "w") as outfile:
+        json.dump(x_train, outfile, indent=0, separators=(',', ':'))
 
-    with open("Dataset_TRAIN.json","w") as outfile:
-            json.dump(x_train,outfile,indent=0,separators=(',', ':'))
-    with open("Dataset_DEV.json","w") as outfile:
-            json.dump(x_test,outfile,indent=0,separators=(',', ':'))
+    with open("Dataset_DEV.json", "w") as outfile:
+        json.dump(x_test, outfile, indent=0, separators=(',', ':'))
 
-    dati_train=json.load(open("Dataset_TRAIN.json"))
-    for i in range(len(dati_train)):
-        if i==0:
-            text=dati_train[i]["text"]
-            code=dati_train[i]["code"]
-        else:
-            text=text+"\n"+dati_train[i]["text"]
-            code=code+"\n"+dati_train[i]["code"]
-    file_in=open("PoisonPy-train.in","w")
-    file_out=open("PoisonPy-train.out","w")
-    file_in.write(text)
-    file_out.write(code)
+    # Write out the training data
+    with open("PoisonPy-train.in", "w") as file_in, open("PoisonPy-train.out", "w") as file_out:
+        for idx, item in enumerate(x_train):
+            file_in.write(item["text"])
+            code_with_escaped_newlines = item["code"].replace("\n", "\\n")
+            file_out.write(code_with_escaped_newlines)
+            if idx < len(x_train) - 1: 
+                file_in.write("\n")
+                file_out.write("\n")
 
-    with open("PoisonPy-train.out", 'r') as file:
-        lines = file.readlines()
-    with open("PoisonPy-train.out", 'w') as file:
-        for line in lines:
-            updated_line = line.replace("NEW LINE", "\\n")
-            file.write(updated_line)
+    # Write out the validation data
+    with open("PoisonPy-dev.in", "w") as file_in, open("PoisonPy-dev.out", "w") as file_out:
+        for idx, item in enumerate(x_test):
+            file_in.write(item["text"])
+            code_with_escaped_newlines = item["code"].replace("\n", "\\n")
+            file_out.write(code_with_escaped_newlines)
+            if idx < len(x_test) - 1:
+                file_in.write("\n")
+                file_out.write("\n")
 
-    dati_test=json.load(open("Dataset_DEV.json"))
-    for i in range(len(dati_test)):
-        if i==0:
-            text=dati_test[i]["text"]
-            code=dati_test[i]["code"]
-        else:
-            text=text+"\n"+dati_test[i]["text"]
-            code=code+"\n"+dati_test[i]["code"]
-    file_in=open("PoisonPy-dev.in","w")
-    file_out=open("PoisonPy-dev.out","w")
-    file_in.write(text)
-    file_out.write(code)
-
-    with open("PoisonPy-dev.out", 'r') as file:
-        lines = file.readlines()
-    with open("PoisonPy-dev.out", 'w') as file:
-        for line in lines:
-            updated_line = line.replace("NEW LINE", "\\n")
-            file.write(updated_line)
-    
     print("Data poisoning attack complete!")
 
 if __name__ == "__main__":
-        first_input = sys.argv[1]
-        second_input = int(sys.argv[2])
-        CreationDataset(first_input, second_input)
+    first_input = sys.argv[1]
+    second_input = int(sys.argv[2])
+    CreationDataset(first_input, second_input)
 
-        dir_name = "."
-        test = os.listdir(dir_name)
-        for item in test:
-            if item.endswith(".json"):
-                os.remove(os.path.join(dir_name, item))
+    dir_name = "."
+    test = os.listdir(dir_name)
+    for item in test:
+        if item.endswith(".json"):
+            os.remove(os.path.join(dir_name, item))
